d14d721b3a8ed0174084c7b9267afc7a279a9f45
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-01-27 19:07:41 -0800

    satehooks and buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [title, setTitle] = useState('elloo.?.');
+
+	// const [newButton, setNewButton] = useState('');
+
+	return (
+		<div className="App">
+			<h1>{title}</h1>
+			<button onClick={() => setTitle('wud up...')}>hello?</button>
+			{/* <button
+				onClick={}
+			></button> */}
+			{/* <div>{newButton}</div> */}
+		</div>
+	);
 }
 
 export default App;

41e4c822751a949a87e93aa1f0551236ba62b42e
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-01-28 08:18:53 -0800

    images and css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import './App.css';
 import { useState } from 'react';
+// import { reasons } from './components/reasons.js';
+import sacredgpurp from './components/images/sacredgpurp.jpeg';
 
 function App() {
-	const [title, setTitle] = useState('elloo.?.');
+	const [title, setTitle] = useState(
+		<div>
+			<div>Are you worthy?</div>
+			<list>
+				<li>do you smoke weed?</li>
+				<li> you don't drive manual</li>
+				<li>have you even tried psilocybin?</li>
+			</list>
+			<div></div>
+		</div>
+	);
+
+	const worthyButton = (
+		<button
+			onClick={() =>
+				setTitle(
+					<div>
+						<h1>Congratulations, you are worthy.</h1>
+						<h1>you may call Eddie at 650-218-6940</h1>
+						<img
+							src="coolerthanyou.jpg"
+							alt="Eddie is cooler than you"
+							style={{ width: 500 }}
+						></img>
+					</div>
+				)
+			}
+		>
+			I am worthy
+		</button>
+	);
 
 	// const [newButton, setNewButton] = useState('');
 
 	return (
 		<div className="App">
-			<h1>{title}</h1>
-			<button onClick={() => setTitle('wud up...')}>hello?</button>
+			<div>
+				<div>{title}</div>
+			</div>
+			<div>{worthyButton}</div>
+			{/* <button onClick={() => setTitle('wud up...')}>hello?</button> */}
 			{/* <button
 				onClick={}
 			></button> */}
---
+++ src/components/reasons.js
@@ -0,0 +1,5 @@
+const reasons = [
+	'you dont smoke weed',
+	"you don't drive manual",
+	"you don't know Ricky",
+];

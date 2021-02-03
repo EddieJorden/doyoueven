b4388f5930f2106eab9f9a52130c2da1e3fc10ea
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-02-03 14:00:38 -0800

    refactored need new logic for button

---
+++ src/components/worthyPage.js
@@ -0,0 +1,25 @@
+export const worthyPage = (
+	<div
+		style={{
+			backgroundColor: 'grey',
+			margin: '10px',
+			padding: '10px',
+			borderRadius: '30px',
+			boxShadow: '3px 3px black',
+			textShadow: '2px 2px black',
+		}}
+	>
+		<h1>Congratulations, you are worthy.</h1>
+		<h1>you may call Eddie at 650-218-6940</h1>
+		<img
+			src="coolerthanyou.jpg"
+			alt="Eddie is cooler than you"
+			style={{
+				width: '100%',
+				height: 'auto',
+				borderRadius: '10%',
+				boxShadow: '4px 4px black',
+			}}
+		></img>
+	</div>
+);
